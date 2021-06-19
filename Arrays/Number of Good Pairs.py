11799b026e08275b3aea9127ede3499731ec8324
aryanag7 <62555290+aryanag7@users.noreply.github.com>
2021-06-19 11:07:17 +0530

    Update Number of Good Pairs.py
    2nd commit

--- Arrays/Number of Good Pairs.py
+++ Arrays/Number of Good Pairs.py
@@ -6,5 +6,12 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[i]==nums[j] and i<j:
                     count=count+1
-        return count      
+        return count    
+    # 2nd answer
+     nums=sorted(nums)
+        dict=Counter(nums)
+        ans=0
+        for val in dict.values():
+            ans=ans+(val**2-val)//2
+        return ans    
         
