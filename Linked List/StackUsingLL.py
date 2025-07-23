f9114a52527eb444c5093dbeca4cc475791ce62e
Aryan Agrawal <aryanagrawal2310@gmail.com>
2025-07-23 11:11:18 -0400

    reverse LL

--- Linked List/StackUsingLL.py
+++ Linked List/StackUsingLL.py
@@ -59,6 +59,7 @@ s.push(20)
 s.push(30)
 s.push(40)
 s.push(50)
+s.push(60)
 
 
 s.print_stack()  # Output: 30 -> 20 -> 10 -> None
---
+++ Linked List/reverseLL.py
@@ -0,0 +1,83 @@
+import bisect
+import sys
+import os
+import math
+from collections import defaultdict
+from collections import Counter
+from typing import Deque
+from collections import deque
+from itertools import accumulate
+
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Redirect stdin and stdout
+sys.stdin = open(os.path.join(current_dir, 'input.txt'), 'r')
+sys.stdout = open(os.path.join(current_dir, 'output.txt'), 'w')
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+
+    # TC -> O(N)
+    # SC -> O(1)
+
+    def reverseList(self, head):
+        if head is None or head.next is None:
+            return head
+        
+        curr=head
+        forward=curr
+        prev=None
+
+        while curr is not None:
+            forward=curr.next
+            curr.next=prev
+            prev=curr
+            curr=forward
+
+        
+        head=prev
+        return head
+    
+    def take_input(self):
+        head=None
+        tail=None
+
+        arr=list(map(int,input().split(" ")))
+        for ele in arr:
+            if ele ==-1:
+                break
+            new_node= ListNode(ele)
+            if head is None and tail is None:
+                head=new_node
+                tail=new_node
+            else:
+                tail.next= new_node
+                tail=tail.next
+
+        return head
+
+    def printLL(self,head):
+        curr=head
+        while curr:
+            print(curr.val,end=" -> ")
+            curr=curr.next
+
+        print('None')
+    
+    
+s1= Solution()
+head=s1.take_input()
+reversed_head = s1.reverseList(head)
+s1.printLL(reversed_head)
+
+
+
+
+
+
+
