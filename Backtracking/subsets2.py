0c966fb8d2699fe78af8de65fbd30d5655c102dc
Aryan Agrawal <aryanagrawal2310@gmail.com>
2025-07-31 15:32:42 -0400

    updated

---
+++ Backtracking/subsets2.py
@@ -0,0 +1,45 @@
+import bisect
+import sys
+import os
+import math
+from collections import defaultdict
+from collections import Counter
+from typing import Deque
+from collections import deque
+from itertools import accumulate
+
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Redirect stdin and stdout
+sys.stdin = open(os.path.join(current_dir, 'input.txt'), 'r')
+sys.stdout = open(os.path.join(current_dir, 'output.txt'), 'w')
+
+
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+s1 = Solution()
+nums = list(map(int, input().split()))
+print(s1.subsets(nums))
+
+    
+
+
