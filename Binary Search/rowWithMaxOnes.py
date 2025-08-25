ca0f7b7b7d41af3e3521bb73d214389daa2cfca0
Aryan Agrawal <aryanagrawal2310@gmail.com>
2025-08-24 20:55:21 -0400

    BS on matrix

---
+++ Binary Search/rowWithMaxOnes.py
@@ -0,0 +1,77 @@
+import bisect
+import sys
+import os
+import math
+from collections import defaultdict
+from collections import Counter
+from typing import Deque
+from collections import deque
+from itertools import accumulate
+
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Redirect stdin and stdout
+sys.stdin = open(os.path.join(current_dir, 'input.txt'), 'r')
+sys.stdout = open(os.path.join(current_dir, 'output.txt'), 'w')
+
+
+class Solution:
+    # TC:- O(N* M)
+    # SC:- O(1)
+    def rowAndMaximumOnes(self, mat):
+        n=len(mat)
+        m=len(mat[0])
+        ans=[-1,-1]
+
+        for i in range(0,n):
+            ones = 0
+            for j in range(0,m):
+                if mat[i][j]==1:
+                    ones+=1
+
+            if ones>ans[1]:
+                ans[1]= ones
+                ans[0]=i
+        
+        return ans
+    
+
+    # TC:- O(N * (M LOG M + LOG(M)))) above is the better one 
+    # SC:- O(1)
+    def rowAndMaximumOnes(self, mat):
+        n=len(mat)
+        m=len(mat[0])
+        ans=[0,0]
+
+        for i in range(0,n):
+            row = mat[i]
+            row.sort()
+            num_ones = bisect.bisect_left(row,1)
+            if num_ones!=m and m-num_ones > ans[1]:
+                ans[1] = m-num_ones
+                ans[0]=i
+
+        return ans 
+
+
+
+            
+        
+
+            
+
+
+
+
+
+
+s1 = Solution()
+mat = [[1,1,1,1,1,],[0,0,0,0,0],[1,1,1,1,1],[0,0,0,0,0],[0,1,1,1,1]]
+print(s1.rowAndMaximumOnes(mat))
+
+    
+
+
+
+
