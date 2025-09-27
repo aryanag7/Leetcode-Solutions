ed5df77c960f072366496a3f3465313fdb1d8ceb
Aryan Agrawal <aryanagrawal2310@gmail.com>
2025-09-27 19:55:57 -0400

    updated

--- Trees/Binary Tree Maximum Path Sum.py
+++ Trees/Binary Tree Maximum Path Sum.py
@@ -1,21 +1,74 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def help(self,root,maxi):
-        if root==None:
-            return 0
-        left=max(0,self.help(root.left,maxi))
-        right=max(0,self.help(root.right,maxi))
+import bisect
+import sys
+import os
+import math
+from collections import defaultdict
+from collections import Counter
+from typing import Deque
+from collections import deque
+from itertools import accumulate
 
-        maxi[0]=max(maxi[0],left+right+root.val)
-        return root.val+max(left,right)
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        maxi=[root.val]
-        self.help(root,maxi)
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Redirect stdin and stdout
+sys.stdin = open(os.path.join(current_dir, 'input.txt'), 'r')
+sys.stdout = open(os.path.join(current_dir, 'output.txt'), 'w')
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def take_input(self):
+        root_data=int(input())
+        if root_data==-1:
+            return None
+        
+
+        root=TreeNode(root_data)
+
+        root.left= self.take_input()
+
+        root.right= self.take_input()
+
+        return root
+    
+
+
+    
+    def maxPathSumHelper(self,root, maxi):
+        if root is None:
+            return 0
+        
+        left_sum = self.maxPathSumHelper(root.left,maxi)
+        right_sum = self.maxPathSumHelper(root.right,maxi)
+
+        maxi[0] = max(maxi[0], root.val+ left_sum+ right_sum)
+
+        return root.val + max(left_sum,right_sum)
+    
+
+    # TC:- O(N)
+    # SC:- O(N)
+    def maxPathSum(self, root):
+        maxi = [float('-inf')]
+
+        self.maxPathSumHelper(root,maxi)
+
         return maxi[0]
-        
-        
+
+
+
+
+    
+s1= Solution()
+root = s1.take_input()
+print(s1.maxPathSum(root))
+
+
+
+
