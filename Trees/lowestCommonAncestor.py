21cb499bd57c87ce72f8d5af0a8c0dcc4889d027
Aryan Agrawal <aryanagrawal2310@gmail.com>
2025-09-29 21:36:54 -0400

    updated

---
+++ Trees/lowestCommonAncestor.py
@@ -0,0 +1,65 @@
+import bisect
+import sys
+import os
+import math
+from collections import defaultdict
+from collections import Counter
+from typing import Deque
+from collections import deque
+from itertools import accumulate
+
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Redirect stdin and stdout
+sys.stdin = open(os.path.join(current_dir, 'input.txt'), 'r')
+sys.stdout = open(os.path.join(current_dir, 'output.txt'), 'w')
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def take_input(self):
+        root_data=int(input())
+        if root_data==-1:
+            return None
+        
+
+        root=TreeNode(root_data)
+
+        root.left= self.take_input()
+
+        root.right= self.take_input()
+
+        return root
+    
+
+    def lowestCommonAncestor(self, root, p, q):
+        if root is None:
+            return None
+        
+        if root.val == p or root.val ==q:
+            return root.val
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left is not None and right is not None:
+            return root.val
+        elif left is None:
+            return right
+        else:
+            return left
+
+    
+s1= Solution()
+root = s1.take_input()
+print(s1.lowestCommonAncestor(root,5,1))
+
+
+
+
