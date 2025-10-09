32aa7f7e8ccc1899d8d43d90ac8d38809b5d4ab1
Aryan Agrawal <aryanagrawal2310@gmail.com>
2025-10-08 20:22:10 -0400

    updated

---
+++ Heaps/kthSmallestElementMatrix.py
@@ -0,0 +1,90 @@
+import bisect
+import sys
+import os
+import math
+from collections import defaultdict
+from collections import Counter
+from typing import Deque
+from collections import deque
+from itertools import accumulate
+import heapq
+
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Redirect stdin and stdout
+sys.stdin = open(os.path.join(current_dir, 'input.txt'), 'r')
+sys.stdout = open(os.path.join(current_dir, 'output.txt'), 'w')
+
+
+
+class Solution:
+  # TC :- o(N*N) + O(N*N log N*N)
+  # SC:- O(N*N)
+  def kthSmallest(self, matrix, k):
+    arr = []
+    for row in matrix:
+      for ele in row:
+        arr.append(ele)
+    
+    arr.sort()
+
+    return arr[k-1]
+  
+  #could have also pushed all nodes in pq, n*n log n*n and then popped k times k * log (n*n)
+  
+
+  # TC :- o(N*N log K) - heap size is K
+  # SC:- O(K)
+  def kthSmallest(self, matrix, k):
+    maxHeap = []
+
+    for row in matrix:
+      for ele in row:
+          heapq.heappush(maxHeap,-ele)
+
+          if len(maxHeap) > k:
+            heapq.heappop(maxHeap)
+    
+    return -maxHeap[0]
+  
+
+
+
+
+  #K - WAY MERGE SIMILAR TO MERGE K SORTED LISTS
+  # TC:- O(N * logN) + O(K * log(N))
+  # SC:- O(N) - N is number of rows
+  def kthSmallest(self, matrix, k):
+    n= len(matrix)
+    m= len(matrix[0])
+
+    minHeap = []
+
+
+    #put each of min from all the sorted lists to keep size O(N) - no of rows
+    for i in range(0,n):
+      heapq.heappush(minHeap, (matrix[i][0],i,0))
+    
+    i=0
+    #pop k times - 1st smallest, 2nd, 3rd .... kth smallest
+    for i in range(0,k):
+      cell, row, col = heapq.heappop(minHeap)
+      i+=1
+      
+      if col + 1 < m:
+        heapq.heappush(minHeap, (matrix[row][col+1],row,col+1))
+    
+    return cell
+
+  
+  
+
+
+s1= Solution()
+matrix = [[1,5,9],[10,11,13],[12,13,15]]
+k = 8
+print(s1.kthSmallest(matrix,k))
+
+
+
---
+++ Heaps/mergeKSortedLists.py
@@ -0,0 +1,126 @@
+import bisect
+import sys
+import os
+import math
+from collections import defaultdict
+from collections import Counter
+from typing import Deque
+from collections import deque
+from itertools import accumulate, count
+import heapq
+
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Redirect stdin and stdout
+sys.stdin = open(os.path.join(current_dir, 'input.txt'), 'r')
+sys.stdout = open(os.path.join(current_dir, 'output.txt'), 'w')
+
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        
+        dummyNode= ListNode(-1)
+        prev=dummyNode
+
+        while list1 and list2:
+            if list1.val<=list2.val:
+                prev.next= list1
+                list1=list1.next
+            else:
+                prev.next=list2
+                list2=list2.next
+            prev=prev.next
+            
+        if list1:
+            prev.next=list1
+
+        if list2:
+            prev.next=list2
+        
+        return dummyNode.next
+    
+    # TC:- O(NlogN) - N is total nodes of all lists pushed into minheap and popped out N times       N = k * N ->  k lists each of size N assuming
+    # SC:- O(k*N) (for minHeap) + O(K*N) (for ans)
+    #better than pairwise merge using merge 2 lists TC : O(K*k*N)
+    def mergeKLists(self, lists):
+        if len(lists)==0:
+            return None
+
+        minHeap = []
+        for list in lists:
+            head= list
+            while head:
+                heapq.heappush(minHeap, (head.val))
+                head = head.next
+        
+        dummyNode = ListNode(-1)
+        prev  = dummyNode
+        while len(minHeap)>0:
+            nodeVal = heapq.heappop(minHeap)
+            node = ListNode(nodeVal)
+            prev.next = node
+            prev=prev.next
+        
+        return dummyNode.next
+    
+
+
+
+
+
+    # TC:- O(KlogK + k*N log K) - N is size of each list and K is number of lists
+    # SC:- O(k*N) + O(K*N)
+    #heap size if K instead of K*N
+    def mergeKLists(self, lists):
+        if len(lists)==0:
+            return None
+        
+        tie = count()  
+        
+        minHeap = []
+        #k - size of lists
+        for li in lists:
+            heapq.heappush(minHeap, (li.val,next(tie),li))
+        
+        prev = dummy = ListNode(-1)
+
+    # K*N log K - as you would be ultimately pushing all nodes into heap but heap size is only K
+        while len(minHeap) >0:
+            val, node = heapq.heappop(minHeap)
+
+            prev.next = node
+            prev=prev.next
+
+            if node.next:
+                heapq.heappush(minHeap, (node.next.val,next(tie),node.next))
+        
+        return dummy.next
+
+                
+        
+   
+
+
+
+        
+            
+s1= Solution()
+arr = [1,2,3,4,5]
+k = 4
+x = 3
+print(s1.findClosestElements(arr,k,x))
+
+
+
